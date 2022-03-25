7e37262f2060ed1ed7bd1378faae5ebe4429e32e
NicoletteSD <101572669+NicoletteSD@users.noreply.github.com>
2022-03-25 08:39:09 -0500

    UPDATE

--- PhoneBookApplication/src/Main.java
+++ PhoneBookApplication/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		PhoneBookApplication phonebook = new PhoneBookApplication();
+		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 		int response;
 
